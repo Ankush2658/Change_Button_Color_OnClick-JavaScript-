e9fa556d80feccc4a7fd11ad8cc9cbd24886da41
Ankush Aglawe <ankushaglawe123@gmail.com>
2021-06-20 00:00:56 +0530

    first commit

---
+++ changeButtonColor.js
@@ -0,0 +1,66 @@
+
+    function ChangeColor()
+    {
+        document.getElementById('btn1').style.backgroundColor = 'Red';
+        
+        document.getElementById("btn1").reset();
+    }    
+    function ChangeColor2()
+    {
+        document.getElementById('btn1').style.backgroundColor = 'Green';
+        
+        document.getElementById("btn1").reset();
+    }
+    function ChangeColor3()
+    {
+        document.getElementById('btn1').style.backgroundColor = 'Yellow';
+        
+        document.getElementById("btn1").reset();
+    }
+    function reset()
+    {
+        document.getElementById("btn1").reset();
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+               function ChangeColor()  
+               {  
+                    document.getElementById('btn1').style.backgroundColor='Red';  
+                    
+                    document.getElementById("btn4").reset();
+               }                 
+               function ChangeColor2()  
+               {  
+                    document.getElementById('btn2').style.backgroundColor='Green';  
+                    
+                    document.getElementById("btn4").reset();  
+               }  
+               function ChangeColor3()  
+               {  
+                    document.getElementById('btn3').style.backgroundColor='Yellow';
+                    
+                    document.getElementById("btn4").reset();  
+               }  
+
+
+// function ChangeColor()
+//     {
+//         document.getElementById('btn1').style.backgroundColor = 'Red';
+
+//     }    
+//     function ChangeColor2()
+//     {
+//         document.getElementById('btn1').style.backgroundColor = 'Green';
+        
+//     }
+//     function ChangeColor3()
+//     {
+//         document.getElementById('btn1').style.backgroundColor = 'Yellow';
+        
+//     }
+//     function reset()
+//     {
+//         document.getElementById("btn1").reset();
+//     }
